0e9d655338504e04fb744fc31320b098f26fc8b1
Arrif.Sondjalim <arrif.sondjalim@hm.edu>
2025-10-29 12:09:05 +0100

    CPU: UI in ASCII-format was implemented correctly, only Interactive version remains to be programmed

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 
 #include "utils/cpu.hpp"
+#include "utils/ui_ascii.hpp"
 
 int main() {
-  std::cout << "=== MONITORING INFORMATION ===\n";
-  observer::cpu::ShowAllCPUTemperatures();
-  std::cout << observer::cpu::GetAverageCPUTemperature() << std::endl;
-  return 0;
+  std::cout << "Choose Mode:\n";
+  std::cout << "[1] BASIC" << " or " << "[2] REAL-TIME" << std::endl;
+  int choice;
+  std::cin >> choice;
+
+  if (choice == 1)
+    observer::ShowAsciiUI(false);
+  else if (choice == 2)
+    observer::ShowAsciiUI(true);
 }
